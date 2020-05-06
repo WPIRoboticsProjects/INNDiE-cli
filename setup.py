b201355437d5b429e59af3f866dac5d43cc75dc4
Octogonapus <firey45@gmail.com>
2020-05-06 12:45:15 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="inndie",
-    version="0.1.17",
+    version="0.1.18",
     description="The INNDiE CLI",
     long_description=long_description,
     long_description_content_type="text/markdown",
